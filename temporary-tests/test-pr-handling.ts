559e8d3601a6cdb6641d0b9f30dc82667595bb84
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2021-05-25 14:25:11 +0200

    Create test-pr-handling.ts

---
+++ temporary-tests/test-pr-handling.ts
@@ -0,0 +1,10 @@
+export function requestCLASignature(param1: string) {
+    // tbd - e.g. using https://github.com/marketplace/actions/cla-assistant-lite
+    // CLAAssistant.requestCLASignature()
+
+    console.log(`hello world - ${param1}`)
+}
+
+const param1 = Deno.args[0]
+
+requestCLASignature(param1)
